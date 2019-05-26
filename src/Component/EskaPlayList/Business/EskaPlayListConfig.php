efca8fa8f962fef53a96a3b2f32e2eeeab96e961
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2019-05-26 23:21:58 +0200

    spotify

---
+++ src/Component/EskaPlayList/Business/EskaPlayListConfig.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+
+namespace App\Component\EskaPlayList\Business;
+
+
+interface EskaPlayListConfig
+{
+    public const REFRESH_TOKEN_FILE_NAME = 'token.txt';
+}
---
+++ src/Component/EskaPlayList/Business/Factory/SpotifyWebApi.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+
+namespace App\Component\EskaPlayList\Business\Factory;
+
+
+use App\Component\EskaPlayList\Business\EskaPlayListConfig;
+use SpotifyApiConnect\Application\SpotifyWebApiInterface;
+use SpotifyApiConnect\SpotifyApiConnectFactoryInterface;
+
+class SpotifyWebApi
+{
+    /**
+     * @var SpotifyApiConnectFactoryInterface
+     */
+    private $spotifyApiConnectFactory;
+
+    /**
+     * @var
+     */
+    private $kernelProjectDir;
+
+    /**
+     * @return \SpotifyApiConnect\Application\SpotifyWebApiInterface
+     */
+    public function createSpotifyWebApi() : SpotifyWebApiInterface
+    {
+        $accessToken = '';
+        $spotifyApiAuth = $this->spotifyApiConnectFactory->createSpotifyApiAuth();
+        $tokenPath = $this->kernelProjectDir . '/'.  EskaPlayListConfig::REFRESH_TOKEN_FILE_NAME;
+        if (file_exists($tokenPath) && ($token = file_get_contents($tokenPath))) {
+            $accessToken = $spotifyApiAuth->getAccessByRefreshToken($token);
+        } else {
+            trigger_error('Token file not found ' . $tokenPath, E_USER_WARNING);
+        }
+
+        return $this->spotifyApiConnectFactory->createSpotifyWebApi($accessToken);
+    }
+}
